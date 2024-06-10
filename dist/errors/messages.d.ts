c277dc7e0f7603ec270ad5d13bfa9c255e0a4130
mostafa <mostafadarvishi@gmail.com>
2024-06-10 10:04:09 +0330

    Init

---
+++ dist/errors/messages.d.ts
@@ -0,0 +1,3 @@
+export declare enum ErrorMessages {
+    'userNotFound' = "user not found"
+}
---
+++ dist/errors/messages.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ErrorMessages = void 0;
+var ErrorMessages;
+(function (ErrorMessages) {
+    ErrorMessages["userNotFound"] = "user not found";
+})(ErrorMessages || (exports.ErrorMessages = ErrorMessages = {}));
